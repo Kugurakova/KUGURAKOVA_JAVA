a2fdcfca7cb905511c25f6edd1ed98078b6e21e6
Kugurakova <akugurak@gmail.com>
2019-01-05 12:17:57 +0300

    tasks 2 and 13

--- Tasks/Task 02/program.java
+++ Tasks/Task 02/program.java
@@ -1,27 +1,8 @@
 import java.util.Scanner;
 class Program {
-	public static int stepen(int n,int s) {
-		int result =1;
-		for(int i=1; i<=s; i++){
-			result = result*n;
-		}
-		return result;
-	}
-
-	public static int BinarySystem(int n) {
-		int x=0;
-		for (int i=1 ;i < 9; i++){
-			x = x + n % 2 * stepen(10,i-1);
-			n = n / 2;
-		}
-		return x; 
-	}	
-
 	public static void main(String[] args) {
 	 	Scanner scanner = new Scanner(System.in);
 	 	int n = scanner.nextInt();
-		System.out.println(n);
-		System.out.println(BinarySystem(n));
-		
+		System.out.println(""+ n /128 % 2 +""+ n / 64 % 2 +""+ n / 32 % 2+""+ n / 16 % 2 +""+ n / 8 % 2 +""+ n / 4 %2 +""+ n /2 % 2 +""+ n % 2);
 	}
 }
---
+++ Tasks/Task 13/program.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Program {
+	public static int stepen(int n,int s) {
+		int result =1;
+		for(int i=1; i<=s; i++){
+			result = result*n;
+		}
+		return result;
+	}
+
+	public static int BinarySystem(int n) {
+		int x=0;
+		for (int i=1 ;i < 9; i++){
+			x = x + n % 2 * stepen(10,i-1);
+			n = n / 2;
+		}
+		return x; 
+	}	
+
+	public static void main(String[] args) {
+	 	Scanner scanner = new Scanner(System.in);
+	 	int n = scanner.nextInt();
+		System.out.println(BinarySystem(n));
+		
+	}
+}
